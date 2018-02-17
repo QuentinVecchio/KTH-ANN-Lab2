c39d4862089fe5ce42c38d0b6b8763d652af0614
Quentin Vecchio <vecchioquentin@hotmail.fr>
2018-02-17 19:00:47 +0100

    Add the competitive learning and scenario 3.3

--- src/RBF.py
+++ src/RBF.py
@@ -7,20 +7,38 @@ import numpy as np
 class simpleRBF():
 #   Implements a RBF with 1 input and 1 output
 
-    def __init__(self, lr=0.001, nb_eboch=20, batch_size=-1, nb_hidden=5):
+    def __init__(self, lr=0.001, nb_eboch=20, batch_size=1, nb_hidden=5):
         self.batch_size = batch_size
         self.lr = lr
         self.nb_eboch = nb_eboch
         self.nb_hidden = nb_hidden
         self.W = []
-        self.mu = []
+        self.mu = np.reshape(np.array(np.linspace(0, 2*np.pi, num=self.nb_hidden)) , (1,self.nb_hidden))
         self.sigma = []
+    def initialisationMu(self,X, nb_ite=100):
+        print(self.mu.shape)
+        p = np.random.randint(len(X),size=nb_ite)
+        for index in p:
+            #print(index)
+            current = X[index]
+            best_index = 0;
+            best_value = 100000000.0;
+            for index_mu in range(len(self.mu[0])):
+                node_mu = self.mu[0][index_mu]
+                value = np.linalg.norm([current - node_mu], ord=2)
+                if(value < best_value):
 
+                    best_value = value
+                    best_index = index_mu
+
+            self.mu[0][best_index] = self.mu[0][best_index] + self.lr * (current - self.mu[0][best_index])
+        print(self.mu)
     def errorGivenPrediction(self, K, T):
         return sum(np.square(K-T))
 
     def error(self, X, T):
-        return np.sum(np.square(self.predict(X)-T))
+        #print(str(self.predict(X).shape) + " " + str(T.shape))
+        return np.sum((self.predict(X)-T))# / len(X)*1.0
 
     def getMatricePhi(self, X):
         PHI = np.ones((len(X),self.nb_hidden+1))
@@ -41,12 +59,11 @@ class simpleRBF():
         bestW = self.W[:]
         bestError = np.inf
 
-        if len(mu) == 0:
-            self.mu = np.reshape(np.random.uniform(low=-10, high=10, size=self.nb_hidden), (1, self.nb_hidden))
-        else:
+        
+        if(len(mu) >0): 
             self.mu = np.array(mu)
         self.sigma = np.array([sigma] * (self.nb_hidden))
-
+        print(self.mu)
         errorHistory = []
         PHI = self.getMatricePhi(X)
 
@@ -56,7 +73,7 @@ class simpleRBF():
 
             if step%(self.nb_eboch/10) == 0:
                 print("Step : " + str(step))
-                print("Current error : " + str(self.error(X,T)))
+                print("Current error : " + str(abs(self.error(X,T))))
 
             batchIndex_list =[]
             if(self.batch_size == -1):
@@ -85,11 +102,11 @@ class simpleRBF():
                 deltaW = np.reshape(deltaW, (self.nb_hidden + 1,1))
                 #print(deltaW.shape)
                 self.W += deltaW
-
-            if abs(self.error(X,T)) < bestError:
-                bestError = abs(self.error(X,T))
+            errorAux = abs(self.error(X,T))
+            if abs(errorAux) < bestError:
+                bestError = abs(errorAux)
                 bestW = self.W[:]
-            errorHistory.append(self.error(X,T))
+            errorHistory.append(errorAux)
 
         self.W = bestW
         print("Best error : " + str(bestError))
--- src/scenarios.py
+++ src/scenarios.py
@@ -62,7 +62,7 @@ def scenario3_2():
     YSinus = YSinus.reshape((len(YSinus),1))
     YSinusTest = np.sin(2*XTest)
     YSinusTest = YSinusTest.reshape((len(YSinusTest),1))
-    rbfSinus = RBF.simpleRBF(lr=0.2, nb_eboch=500, nb_hidden=NB_HIDDEN, batch_size=-1)
+    rbfSinus = RBF.simpleRBF(lr=0.2, nb_eboch=500, nb_hidden=NB_HIDDEN, batch_size=1)
     history = rbfSinus.fit(X=X, T=YSinus, mu=np.linspace(0, 2*np.pi, num=NB_HIDDEN) , sigma=np.sqrt(2*np.pi/NB_HIDDEN))
     YSinusRBF = rbfSinus.predict(XTest)
     graph.plotRBFInformations("Sinus function with noise", XTest, YSinusTest, YSinusRBF, YSinus, history, [0, 2*np.pi, -1.5, 1.5])
@@ -75,11 +75,41 @@ def scenario3_2():
     YSquare = YSquare.reshape((len(YSquare),1))
     YSquareTest = square(2*XTest)
     YSquareTest = YSquareTest.reshape((len(YSquareTest),1))
-    rbfSquare = RBF.simpleRBF(lr=0.01, nb_eboch=10000, nb_hidden=NB_HIDDEN, batch_size=-1)
+    rbfSquare = RBF.simpleRBF(lr=0.01, nb_eboch=10000, nb_hidden=NB_HIDDEN, batch_size=1, neighbourhood_size=50)
     #history = rbfSquare.fit(X=X, T=YSquare, mu=[0, 2*np.pi, np.pi-0.01, np.pi+0.01, np.pi-0.05, np.pi+0.05] , sigma=np.sqrt(1))
     history = rbfSquare.fit(X=X, T=YSquare, mu=np.linspace(0, 2*np.pi, num=NB_HIDDEN) , sigma=np.sqrt(2*np.pi/NB_HIDDEN))
     YSquareRBF = rbfSquare.predict(XTest)
     graph.plotRBFInformations("Square function with noise", XTest, YSquareTest, YSquareRBF, YSquare, history, [0, 2*np.pi, -1.5, 1.5])
 
 def scenario3_3():
-    print("Not Implemented yet")
+    N = math.ceil(2 * math.pi / 0.1)
+    X = np.linspace(0, 2*np.pi, num=N)
+    XTest = np.linspace(0.05, 2*np.pi, num = N)
+    NB_HIDDEN=10
+    ##### Sinus Function #####
+
+    YSinus = np.sin(2*X)
+    YSinus = YSinus.reshape((len(YSinus),1))
+    YSinusTest = np.sin(2*XTest)
+    YSinusTest = YSinusTest.reshape((len(YSinusTest),1))
+    rbfSinus = RBF.simpleRBF(lr=0.2, nb_eboch=500, nb_hidden=NB_HIDDEN, batch_size=1)
+    rbfSinus.initialisationMu(X)
+    history = rbfSinus.fit(X=X, T=YSinus , sigma=np.sqrt(2*np.pi/NB_HIDDEN))#np.sqrt(2*np.pi/NB_HIDDEN)
+    YSinusRBF = rbfSinus.predict(XTest)
+    graph.plotRBFInformations("Sinus function", XTest, YSinusTest, YSinusRBF, YSinus, history, [0, 2*np.pi, -1.5, 1.5])
+
+    ##### Square Function #####
+
+    N = math.ceil(2 * math.pi / 0.1)
+    X = np.linspace(0, 2*np.pi, num=N)
+    XTest = np.linspace(0.05, 2*np.pi+0.05, num = N)
+    YSquare = square(2*X)
+    YSquare = YSquare.reshape((len(YSquare),1))
+    YSquareTest = square(2*XTest)
+    YSquareTest = YSquareTest.reshape((len(YSquareTest),1))
+    rbfSquare = RBF.simpleRBF(lr=0.2, nb_eboch=10000, nb_hidden=NB_HIDDEN, batch_size=1)
+    rbfSquare.initialisationMu(X)
+    #history = rbfSquare.fit(X=X, T=YSquare, mu=[0, 2*np.pi, np.pi-0.01, np.pi+0.01, np.pi-0.05, np.pi+0.05] , sigma=np.sqrt(1))
+    history = rbfSquare.fit(X=X, T=YSquare , sigma=np.sqrt(2*np.pi/NB_HIDDEN))
+    YSquareRBF = rbfSquare.predict(XTest)
+    graph.plotRBFInformations("Square function", XTest, YSquareTest, YSquareRBF, YSquare, history, [0, 2*np.pi, -1.5, 1.5])
