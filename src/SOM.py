8916a80c9fc2b53c065a4b48a72a6f649b3744b6
Quentin Vecchio <vecchioquentin@hotmail.fr>
2018-02-21 20:58:36 +0100

    Add SOM for TSP and fix bugs in SOM

--- src/SOM.py
+++ src/SOM.py
@@ -29,7 +29,7 @@ class SOM():
 
         # Add the right neighbourhood
         for i in range(int(self.neighbourhood_size)):
-            if (i+winner[1]) >= len(self.W):
+            if (i+winner[1]) >= len(self.W[0]):
                 break
             neighbourhood.append([0,i+winner[1]])
 
@@ -38,16 +38,17 @@ class SOM():
     def neighbourhoodByIndexCircular(self, winner):
         neighbourhood = [winner]
 
+        #1 2 3 4 5
         # Add the left neighbourhood
         for i in range(int(self.neighbourhood_size)):
             index = winner[1]-i
             if index < 0:
-                i = len(self.W) - 1 -i
+                index = len(self.W[0]) - i
             neighbourhood.append([0,index])
 
         # Add the right neighbourhood
         for i in range(int(self.neighbourhood_size)):
-            neighbourhood.append([0,(i+winner[1])%len(self.W)])
+            neighbourhood.append([0,(i+winner[1])%len(self.W[0])])
 
         return neighbourhood
 
@@ -63,7 +64,7 @@ class SOM():
 
         m = np.max(mat)
 
-        for i in range(self.neighbourhood_size*2+1):
+        for i in range(int(self.neighbourhood_size)*2+1):
             t =  np.argmin(mat, axis=1)
             neighbourhood.append(t)
             mat[t[0]][t[1]] = m
@@ -93,30 +94,51 @@ class SOM():
 
     def predictWinnerForInput(self, input):
         bestDistance = self.distance(input, self.W[0][0])
-        winner = (0,0)
+        winner = [0,0]
 
         for x in range(self.W.shape[0]):
             for y in range(self.W.shape[1]):
                 dist = self.distance(input, self.W[x][y])
                 if dist < bestDistance:
                     bestDistance = dist
-                    winner = (x,y)
+                    winner = [x,y]
 
         return winner
 
     def fit(self, X, method):
+        self.output = np.array(self.output)
         firts_neighbourhood_size = self.neighbourhood_size
         self.W = np.random.uniform(0.1, 0.9, (self.output[0], self.output[1], X.shape[1]))
         print(self.W.shape) # 10,15,31
-
+        WHistory = []
+        frequency = np.zeros((self.W.shape[0], self.W.shape[1]))
+        count = 1.0
+        debug = False
         for step in range(self.nb_eboch):
             if step%(self.nb_eboch/10) == 0:
                 print("Epoch " + str(step) + " ...")
 
             # For each input we want to find the closest unit
             for Xk in X:
-                winner = self.predictWinnerForInput(Xk)
+                bestDistance = self.distance(Xk, self.W[0][0])
+                winner = [0,0]
 
+                for x in range(self.W.shape[0]):
+                    for y in range(self.W.shape[1]):
+                        # if self.neighbourhood_size > 0.2:
+                        #     dist = ((frequency[x][y] / count)**2) * self.distance(Xk, self.W[x][y])
+                        # else:
+                        #     if not debug:
+                        #         print("ok")
+                        #         self.lr *= 2
+                        #         debug = True
+                        dist = self.distance(Xk, self.W[x][y])
+                        if dist < bestDistance:
+                            bestDistance = dist
+                            winner = [x,y]
+
+                count += 1
+                frequency[winner[0]][winner[1]] += 1
                 neighbourhood = []
                 if method == "index":
                     neighbourhood = self.neighbourhoodByIndex(winner)
@@ -137,13 +159,139 @@ class SOM():
                     deltaW = self.lr * (Xk - self.W[x][y])
                     self.W[x][y] += deltaW
 
+            WHistory.append(copy.copy(self.W))
+
             # We update the neighbourhood_size to be close of 1
-            #self.neighbourhood_size = self.neighbourhood_size - (firts_neighbourhood_size) / self.nb_eboch
+            self.neighbourhood_size = self.neighbourhood_size - (firts_neighbourhood_size) / self.nb_eboch
+        return WHistory
 
     def predict(self, X):
         result = []
+        coordinates = []
         for index,Xk in enumerate(X):
             winner = self.predictWinnerForInput(Xk)
             result.append(winner)
+            coordinates.append((winner, self.W[winner[0]][winner[1]]))
 
-        return result
+        return result, coordinates
+
+class SOM_TSP():
+
+    def __init__(self, lr=0.001, nb_eboch=20, output=[1,100], neighbourhood_size=50):
+        self.lr = lr
+        self.nb_eboch = nb_eboch
+        self.output = output
+        self.W = []
+        self.neighbourhood_size = neighbourhood_size
+
+    def distance(self, V1, V2):
+        return np.linalg.norm(V1-V2)
+
+    def neighbourhoodByIndexCircular(self, winner):
+        neighbourhood = [winner]
+
+        #1 2 3 4 5
+        # Add the left neighbourhood
+        for i in range(int(self.neighbourhood_size)):
+            index = winner[1]-i
+            if index < 0:
+                index = len(self.W[0]) - i
+            neighbourhood.append([0,index])
+
+        # Add the right neighbourhood
+        for i in range(int(self.neighbourhood_size)):
+            neighbourhood.append([0,(i+winner[1])%len(self.W[0])])
+
+        return neighbourhood
+
+    def predictWinnerForInput(self, input):
+        bestDistance = self.distance(input, self.W[0][0])
+        winner = [0,0]
+
+        for x in range(self.W.shape[0]):
+            for y in range(self.W.shape[1]):
+                dist = self.distance(input, self.W[x][y])
+                if dist < bestDistance:
+                    bestDistance = dist
+                    winner = [x,y]
+
+        return winner
+
+    def fit(self, X, method):
+        self.output = np.array(self.output)
+        firts_neighbourhood_size = self.neighbourhood_size
+        self.W = np.empty((self.output[0], self.output[1], X.shape[1]))
+        ## Warning HARD code
+        N = self.output[1]
+        R = 0.5
+        x, y = X.T
+        centroid = (sum(x) / len(X), sum(y) / len(X))
+        print(centroid)
+        for p in range(N):
+            t = p*(2 * np.pi / N)
+            self.W[0][p] = [R * np.cos(t) + centroid[0], R * np.sin(t) + centroid[1]]
+        print(self.W.shape) # 10,15,31
+        WHistory = []
+        frequency = np.zeros((self.W.shape[0], self.W.shape[1]))
+        count = 1.0
+        debug = False
+        for step in range(self.nb_eboch):
+            if step%(self.nb_eboch/10) == 0:
+                print("Epoch " + str(step) + " ...")
+
+            # For each input we want to find the closest unit
+            for Xk in X:
+                bestDistance = self.distance(Xk, self.W[0][0])
+                winner = [0,0]
+
+                for x in range(self.W.shape[0]):
+                    for y in range(self.W.shape[1]):
+                        if self.neighbourhood_size > 0.2:
+                            dist = ((frequency[x][y] / count)**2) * self.distance(Xk, self.W[x][y])
+                        else:
+                            if not debug:
+                                print("CL finished")
+                                self.lr *= 2
+                                debug = True
+                            dist = self.distance(Xk, self.W[x][y])
+                        if dist < bestDistance:
+                            bestDistance = dist
+                            winner = [x,y]
+
+                count += 1
+                frequency[winner[0]][winner[1]] += 1
+                neighbourhood = []
+                if method == "index":
+                    neighbourhood = self.neighbourhoodByIndex(winner)
+                if method == "index-circular":
+                    neighbourhood = self.neighbourhoodByIndexCircular(winner)
+                if method == "manhattan":
+                    neighbourhood = self.neighbourhoodByManhattan(winner)
+                # elif method == "distance":
+                #     neighbourhood = self.neighbourhoodByDistance(winner)
+                # elif method == "size":
+                #     neighbourhood = self.neighbourhoodBySize(winner)
+                #     neighbourhood = [i[0] for i in neighbourhood]
+
+                # Update the weight of neighbourhood
+                for i in neighbourhood:
+                    x = i[0]
+                    y = i[1]
+                    deltaW = self.lr * (Xk - self.W[x][y])
+                    self.W[x][y] += deltaW
+
+            WHistory.append(copy.copy(self.W))
+
+            # We update the neighbourhood_size to be close of 1
+            self.neighbourhood_size = self.neighbourhood_size - (firts_neighbourhood_size) / self.nb_eboch
+        return WHistory
+
+    def predict(self, X):
+        result = []
+        coordinates = []
+        for index,Xk in enumerate(X):
+            winner = self.predictWinnerForInput(Xk)
+            result.append(winner)
+            coordinates.append((winner, self.W[winner[0]][winner[1]]))
+
+        return result, coordinates
